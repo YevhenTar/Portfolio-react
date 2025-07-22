6557ee93263b0b08df460329058e6a42570c8ce9
YevhenTar <yevhen.tar@gmail.com>
2025-07-22 12:16:45 +0300

    Hide bootstrap-grid from portfolio

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -7,14 +7,14 @@ const MainContent = () => {
     return (
         <main className="main-content">
             <section className="home-section">
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
+                {/*<div className="container">*/}
+                {/*    <div className="row">*/}
+                {/*        <div className="col">*/}
                             <h1 className="home-section__headline">Portfolio</h1>
                             <Slider/>
-                        </div>
-                    </div>
-                </div>
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </section>
         </main>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 import './styles/main.scss';
-import './styles/bootstrap-custom.scss';
+// import './styles/bootstrap-custom.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
