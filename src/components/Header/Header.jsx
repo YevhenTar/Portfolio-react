f91d6cd9c5747f6092837a84760d46bee6260d48
YevhenTar <yevhen.tar@gmail.com>
2025-07-23 11:21:20 +0300

    Update Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Header.scss';
-import { ReactComponent as DownloadIcon } from '../../assets/icons/download.svg';
 
 const Header = () => {
     return (
@@ -8,16 +7,6 @@ const Header = () => {
             <div className="main-header__logo-wrap">
                 <a href="#" className="logo-block">TY</a>
                 <p className="owner-name">Tarasov Yevheniy</p>
-            </div>
-            <div className="main-header__button-wrap">
-                <a
-                    href={`${import.meta.env.BASE_URL}documents/CV_Tarasov-Yevheniy_Front-End-Developer.pdf`}
-                    className="main-header__download-btn"
-                    download="CV_Tarasov-Yevheniy_Front-End-Developer.pdf"
-                >
-                    <DownloadIcon className="download-icon" />
-                    Download CV
-                </a>
             </div>
         </header>
     );
